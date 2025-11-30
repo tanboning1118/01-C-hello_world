590b464e3a2f622de4cfcedf1cabf83fae8af175
Tanboning <8726827@qq.com>
2025-11-30 20:24:42 +0800

    增加输入功能

--- Project1/hello_world.cpp
+++ Project1/hello_world.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include "变量.h"
 #include "常量.h"
-
+using namespace std;
 int main() {
     std::cout << "Hello, world!\n";
+
+    bool flag = false;
+	cout << "请输入Boolean flag value: " <<  std::endl; // 输出布尔变量的值
+	cin >> flag;
+    
+    if (flag) {
+        std::cout << "Flag is true!" << std::endl;
+    } else {
+        std::cout << "Flag is false!" << std::endl;
+	}
 
     // 调用变量示例（如果存在）
     // 如果项目中没有 display()，可以移除此调用
