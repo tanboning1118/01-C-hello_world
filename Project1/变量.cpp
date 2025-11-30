504a6309047f8baf50a8f33718e1178f24d5a4b9
Tanboning <8726827@qq.com>
2025-11-30 17:51:27 +0800

    新增说明注释

--- Project1/变量.cpp
+++ Project1/变量.cpp
@@ -13,7 +13,13 @@ int display() {
 	cout << "字符变量 c 的值: " << c << endl;
 	cout << "布尔变量 d 的值: " << d << endl;
 
-	//下面展示sizeof的使用
+	//下面展示sizeof的使用  sizeof(变量名或数据类型)
+	/* 常用数据类型：
+	整型 short(2) int(4) long(4) long long(8)
+	浮点型 float(4) double(8) long double(16)
+	字符型 char(1)
+	布尔型 bool(1)
+	*/
 	cout << "int 类型的大小: " << sizeof(a) << " 字节" << endl;
 	cout << "double 类型的大小: " << sizeof(double) << " 字节" << endl;
 	cout << "char 类型的大小: " << sizeof(char) << " 字节" << endl;
