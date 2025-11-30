6116923ee9d2fa695c4f3003406223eeccec927f
Tanboning <8726827@qq.com>
2025-11-30 17:48:10 +0800

    新增sizeof关键词用法，查看内存大小

--- Project1/变量.cpp
+++ Project1/变量.cpp
@@ -12,5 +12,12 @@ int display() {
 	cout << "浮点数变量 b 的值: " << b << endl;
 	cout << "字符变量 c 的值: " << c << endl;
 	cout << "布尔变量 d 的值: " << d << endl;
+
+	//下面展示sizeof的使用
+	cout << "int 类型的大小: " << sizeof(a) << " 字节" << endl;
+	cout << "double 类型的大小: " << sizeof(double) << " 字节" << endl;
+	cout << "char 类型的大小: " << sizeof(char) << " 字节" << endl;
+	cout << "bool 类型的大小: " << sizeof(bool) << " 字节" << endl;
+
 	return 0;
 }
