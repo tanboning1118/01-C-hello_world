d5f233cc5355875ab342e83800f97c1da0084d43
Tanboning <8726827@qq.com>
2025-11-30 17:07:12 +0800

    增加了变量的教程文件

---
+++ Project1/变量.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+// 数据类型 变量名  = 初始值;
+int display() {
+	int a = 10; // 整数变量
+	double b = 5.5; // 浮点数变量
+	char c = 'A'; // 字符变量
+	bool d = true; // 布尔变量
+	cout << "整数变量 a 的值: " << a << endl;
+	cout << "浮点数变量 b 的值: " << b << endl;
+	cout << "字符变量 c 的值: " << c << endl;
+	cout << "布尔变量 d 的值: " << d << endl;
+	return 0;
+}
